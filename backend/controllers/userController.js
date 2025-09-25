e69e7da652b7715cfe746b239b83a9888e5518e9
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2025-09-25 18:10:15 +0300

    User Module

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,11 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModels");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
+  // (1) Vaidates if the name,ps,emai written
   if (!name || !password || !email) {
     // create Error Handler instead
     // return res.status(400).json({
@@ -12,6 +15,32 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Missing Feild");
   }
 
+  // (2) Find if exists
+  const userExistes = await User.findOne({ email });
+  if (userExistes) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  // (3) Hashing the PS
+  const salt = await bcrypt.genSalt(10);
+  const hashedPS = await bcrypt.hash(password, salt);
+
+  // (4) creating the user
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPS,
+  });
+
+  // (5) now the user created everything is good and something is created  201
+
+  if (newUser) {
+    res.status(201).json(newUser);
+  } else {
+    res.status(400);
+    throw new Error("incorrect data ");
+  }
   res.json("REGISTER  USER ROUTE  .....");
 });
 
